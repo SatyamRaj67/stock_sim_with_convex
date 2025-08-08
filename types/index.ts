6dd919876bb93e083682413da0e5ff7eec883e87
Satyam Raj <satyamrajgaya434@gmail.com>
2025-08-08 22:01:51 +0530

    .

--- types/index.ts
+++ types/index.ts
@@ -2,24 +2,3 @@ export enum UserRole {
   ADMIN = "ADMIN",
   USER = "USER",
 }
-
-export interface Chapter {
-  id: string;
-  title: string;
-  duration: string;
-  completed: boolean;
-  current?: boolean;
-}
-
-export interface Course {
-  id: string;
-  title: string;
-  description?: string;
-  instructor?: string[];
-  image?: string;
-  level: string;
-  totalDuration?: string;
-  progress: number;
-  tags?: string[];
-  chapters: Chapter[];
-}
