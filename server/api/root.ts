7613eb1fe535370ae738eacd24adf9d8c6cece08
Satyam Raj <satyamrajgaya434@gmail.com>
2025-08-19 18:30:01 +0530

    .

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,4 +1,3 @@
-import { chatRouter } from "@/server/api/routers/chat";
 import { createCallerFactory, createTRPCRouter } from "server/api/trpc";
 
 /**
@@ -7,7 +6,6 @@ import { createCallerFactory, createTRPCRouter } from "server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  chat: chatRouter,
 });
 
 // export type definition of API
